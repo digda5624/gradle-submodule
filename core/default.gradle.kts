d70f0b6a6291b715b32d1cb0eece398e951de594
hyun_digda <hyun_digda@toss.im>
2024-06-29 13:21:39 +0900

    [fix] 중간 저장

--- core/default.gradle.kts
+++ core/default.gradle.kts
@@ -1,16 +1,1 @@
-// build.gradle.kts 에서 buildScript classpath 직접 작성해야 함
-apply(plugin = "java")
-apply(plugin = "kotlin")
-
-subprojects {
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-
-    repositories {
-        mavenCentral()
-    }
-}
-
-repositories {
-    mavenCentral()
-}
+// 일반 task 를 돌려야함
--- core/properties.gradle.kts
+++ core/properties.gradle.kts
@@ -3,5 +3,8 @@ extra["versions"] = mapOf(
     "langs" to mapOf(
         "kotlin" to "1.8.0",
         "java" to "17"
+    ),
+    "libs" to mapOf(
+        "springBoot" to "3.2.3"
     )
 )
